03d03f43ffcda4ea50d667f54a8ee3e864f4cb9f
SpoorthiG <71641003+SpoorthiG@users.noreply.github.com>
2021-08-19 15:04:18 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+	
+	
+	public int addNums(int a, int b){
+		return a+b;
+		
+	}
+	
+	public int multiplyNums(int a, int b){
+		return a*b;
+		
+	}
+	public int subNums(int a, int b){
+		return a-b;
+	}
+	public int divideNums(int a, int b){
+		return a/b;
+	}
+}
